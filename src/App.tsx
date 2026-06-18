71c85cee19871d9e1bbee0cdac5d6607fbee4e02
martinmcathalifaud-del <martinmcathalifaud@gmail.com>
2026-06-17 20:57:08 -0400

    Register vender cupo en dolares route

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import CupoDolaresAPesos from "./pages/CupoDolaresAPesos";
+import VenderCupoDolaresChile from "./pages/VenderCupoDolaresChile";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -18,6 +19,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/cupo-en-dolares-a-pesos-chilenos" element={<CupoDolaresAPesos />} />
+          <Route path="/vender-cupo-en-dolares-chile" element={<VenderCupoDolaresChile />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
